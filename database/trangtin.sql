a0460643088e43f347ded43634aa90ab83040ca0
lamhuy142 <147523526+lamhuy142@users.noreply.github.com>
2024-03-02 10:42:35 +0700

    data

--- database/trangtin.sql
+++ database/trangtin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th3 02, 2024 lúc 02:29 AM
+-- Thời gian đã tạo: Th3 02, 2024 lúc 03:40 AM
 -- Phiên bản máy phục vụ: 8.2.0
 -- Phiên bản PHP: 8.2.13
 
@@ -39,8 +39,11 @@ CREATE TABLE IF NOT EXISTS `baiviet` (
   `TuKhoa` varchar(255) NOT NULL,
   `TrangThaiBV` tinyint NOT NULL,
   `MaLT` int NOT NULL,
-  PRIMARY KEY (`MaBV`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `MaND` int NOT NULL,
+  PRIMARY KEY (`MaBV`),
+  KEY `lt_bv` (`MaLT`),
+  KEY `nd_bv` (`MaND`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -53,8 +56,9 @@ CREATE TABLE IF NOT EXISTS `binhchon` (
   `MaBC` int NOT NULL AUTO_INCREMENT,
   `MaND` int NOT NULL,
   `DanhGia` float NOT NULL,
-  PRIMARY KEY (`MaBC`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`MaBC`),
+  KEY `nd_bc` (`MaND`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -69,8 +73,10 @@ CREATE TABLE IF NOT EXISTS `binhluan` (
   `Ngay` date NOT NULL,
   `MaBV` int NOT NULL,
   `MaND` int NOT NULL,
-  PRIMARY KEY (`MaBL`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`MaBL`),
+  KEY `nd_bl` (`MaND`),
+  KEY `bv_bl` (`MaBV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -80,12 +86,27 @@ CREATE TABLE IF NOT EXISTS `binhluan` (
 
 DROP TABLE IF EXISTS `lichsudoc`;
 CREATE TABLE IF NOT EXISTS `lichsudoc` (
-  `MaLS` int NOT NULL AUTO_INCREMENT,
   `MaND` int NOT NULL,
   `MaBV` int NOT NULL,
   `ThoiGianDoc` datetime NOT NULL,
-  PRIMARY KEY (`MaLS`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `nd_ls` (`MaND`),
+  KEY `bv_ls` (`MaBV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `lichsuthich`
+--
+
+DROP TABLE IF EXISTS `lichsuthich`;
+CREATE TABLE IF NOT EXISTS `lichsuthich` (
+  `MaND` int NOT NULL,
+  `MaBV` int NOT NULL,
+  `NgayThich` date NOT NULL,
+  KEY `nd_lst` (`MaND`),
+  KEY `bv_lst` (`MaBV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -98,7 +119,7 @@ CREATE TABLE IF NOT EXISTS `loaiphanhoi` (
   `MaLPH` int NOT NULL AUTO_INCREMENT,
   `TenLPH` varchar(255) NOT NULL,
   PRIMARY KEY (`MaLPH`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -112,8 +133,9 @@ CREATE TABLE IF NOT EXISTS `loaitin` (
   `TenLT` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `TrangThaiLT` tinyint NOT NULL,
   `MaTL` int NOT NULL,
-  PRIMARY KEY (`MaLT`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`MaLT`),
+  KEY `tl_lt` (`MaTL`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -134,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `nguoidung` (
   `NgayDK` date NOT NULL,
   `TrangThai` tinyint NOT NULL,
   PRIMARY KEY (`MaND`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -147,8 +169,10 @@ CREATE TABLE IF NOT EXISTS `phanhoi` (
   `MaPH` int NOT NULL AUTO_INCREMENT,
   `MaND` int NOT NULL,
   `MaLPH` int NOT NULL,
-  PRIMARY KEY (`MaPH`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`MaPH`),
+  KEY `nd_ph` (`MaND`),
+  KEY `lph_ph` (`MaLPH`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -163,7 +187,21 @@ CREATE TABLE IF NOT EXISTS `quangcao` (
   `URL` varchar(255) NOT NULL,
   `UrlHinhAnh` varchar(255) NOT NULL,
   PRIMARY KEY (`MaQC`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sothichnguoidung`
+--
+
+DROP TABLE IF EXISTS `sothichnguoidung`;
+CREATE TABLE IF NOT EXISTS `sothichnguoidung` (
+  `MaND` int NOT NULL,
+  `MaLT` int NOT NULL,
+  KEY `nd_st` (`MaND`),
+  KEY `lt_st` (`MaLT`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -177,7 +215,87 @@ CREATE TABLE IF NOT EXISTS `theloai` (
   `TenTL` varchar(255) NOT NULL,
   `TrangThai` tinyint NOT NULL,
   PRIMARY KEY (`MaTL`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `thongtingoiy`
+--
+
+DROP TABLE IF EXISTS `thongtingoiy`;
+CREATE TABLE IF NOT EXISTS `thongtingoiy` (
+  `MaND` int NOT NULL,
+  `MaBV` int NOT NULL,
+  `DiemSo` double NOT NULL,
+  KEY `nd_gy` (`MaND`),
+  KEY `bv_gy` (`MaBV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `baiviet`
+--
+ALTER TABLE `baiviet`
+  ADD CONSTRAINT `lt_bv` FOREIGN KEY (`MaLT`) REFERENCES `loaitin` (`MaLT`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `nd_bv` FOREIGN KEY (`MaND`) REFERENCES `nguoidung` (`MaND`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Các ràng buộc cho bảng `binhchon`
+--
+ALTER TABLE `binhchon`
+  ADD CONSTRAINT `nd_bc` FOREIGN KEY (`MaND`) REFERENCES `nguoidung` (`MaND`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Các ràng buộc cho bảng `binhluan`
+--
+ALTER TABLE `binhluan`
+  ADD CONSTRAINT `bv_bl` FOREIGN KEY (`MaBV`) REFERENCES `baiviet` (`MaBV`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `nd_bl` FOREIGN KEY (`MaND`) REFERENCES `nguoidung` (`MaND`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Các ràng buộc cho bảng `lichsudoc`
+--
+ALTER TABLE `lichsudoc`
+  ADD CONSTRAINT `bv_ls` FOREIGN KEY (`MaBV`) REFERENCES `baiviet` (`MaBV`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `nd_ls` FOREIGN KEY (`MaND`) REFERENCES `nguoidung` (`MaND`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Các ràng buộc cho bảng `lichsuthich`
+--
+ALTER TABLE `lichsuthich`
+  ADD CONSTRAINT `bv_lst` FOREIGN KEY (`MaBV`) REFERENCES `baiviet` (`MaBV`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `nd_lst` FOREIGN KEY (`MaND`) REFERENCES `nguoidung` (`MaND`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Các ràng buộc cho bảng `loaitin`
+--
+ALTER TABLE `loaitin`
+  ADD CONSTRAINT `tl_lt` FOREIGN KEY (`MaTL`) REFERENCES `theloai` (`MaTL`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Các ràng buộc cho bảng `phanhoi`
+--
+ALTER TABLE `phanhoi`
+  ADD CONSTRAINT `lph_ph` FOREIGN KEY (`MaLPH`) REFERENCES `loaiphanhoi` (`MaLPH`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `nd_ph` FOREIGN KEY (`MaND`) REFERENCES `nguoidung` (`MaND`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Các ràng buộc cho bảng `sothichnguoidung`
+--
+ALTER TABLE `sothichnguoidung`
+  ADD CONSTRAINT `lt_st` FOREIGN KEY (`MaLT`) REFERENCES `loaitin` (`MaLT`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `nd_st` FOREIGN KEY (`MaND`) REFERENCES `nguoidung` (`MaND`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Các ràng buộc cho bảng `thongtingoiy`
+--
+ALTER TABLE `thongtingoiy`
+  ADD CONSTRAINT `bv_gy` FOREIGN KEY (`MaBV`) REFERENCES `baiviet` (`MaBV`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `nd_gy` FOREIGN KEY (`MaND`) REFERENCES `nguoidung` (`MaND`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
