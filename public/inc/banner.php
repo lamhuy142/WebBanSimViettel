30c20ab5bc7adb8126a2dc47bce17d4039853772
lamhuy142 <147523526+lamhuy142@users.noreply.github.com>
2024-03-08 22:05:10 +0700

    public

--- public/inc/banner.php
+++
@@ -1,5 +0,0 @@
-<div class="col-xl-9 col-lg-9 col-md-9">
-                                <div class="header-banner f-right ">
-                                    <img src="assets/img/hero/header_card.jpg" alt="">
-                                </div>
-                            </div>
