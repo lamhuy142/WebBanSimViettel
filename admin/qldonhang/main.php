aaf862c0313e95e355aca2e7e478b90e5184568a
lamhuy142 <147523526+lamhuy142@users.noreply.github.com>
2024-03-20 13:26:42 +0700

    donhang

--- admin/qldonhang/main.php
+++ admin/qldonhang/main.php
@@ -82,18 +82,18 @@
                                                     <div class="col">
                                                         <a href="index.php?action=khoa&id=<?php echo $dh['MaDH']; ?>&TrangThai=<?php echo $dh['TrangThai']; ?>" class="btn btn-warning">Xác nhận</a>
                                                     </div>
-                                                    <div class="col">
-                                                        <a href="index.php?action=huydon&id=<?php echo $dh['MaDH']; ?>&TrangThai=<?php echo $dh['TrangThai']; ?>" class="btn btn-secondary">Hủy đơn</a>
-                                                    </div>
+                                                    <!-- <div class="col">
+                                                        <a href="index.php?action=huydon&id=<php echo $dh['MaDH']; ?>&TrangThai=<php echo $dh['TrangThai']; ?>" class="btn btn-secondary">Hủy đơn</a>
+                                                    </div> -->
                                                 </div>
                                             <?php } elseif ($dh["TrangThai"] == 1) { ?>
                                                 <div class="row">
                                                     <div class="col">
                                                         <a href="index.php?action=hoantat&id=<?php echo $dh['MaDH']; ?>&TrangThai=<?php echo $dh['TrangThai']; ?>" class="btn btn-warning">Hoàn tất</a>
                                                     </div>
-                                                    <div class="col">
-                                                        <a href="index.php?action=huydon&id=<?php echo $dh['MaDH']; ?>&TrangThai=<?php echo $dh['TrangThai']; ?>" class="btn btn-secondary">Hủy đơn</a>
-                                                    </div>
+                                                    <!-- <div class="col">
+                                                        <a href="index.php?action=huydon&id=<php echo $dh['MaDH']; ?>&TrangThai=<php echo $dh['TrangThai']; ?>" class="btn btn-secondary">Hủy đơn</a>
+                                                    </div> -->
                                                 </div>
                                             <?php } elseif ($dh["TrangThai"] == 2) { ?>
                                                 <div class="row">
